6e3f2c6e048d014721ee002065603b6a78878264
Amith George <strider.am@gmail.com>
2022-08-17 21:26:36 +0530

    extracted the price calculation into a pure function

--- javascript/src/prices.js
+++ javascript/src/prices.js
@@ -30,6 +30,44 @@ function compute_day_of_week_discount(holidays, date) {
   return reduction;
 }
 
+function compute_final_price(holidays, plan, { type, date, age }) {
+  if (age < 6) {
+    return 0;
+  }
+
+  if (type === "night") {
+    if (age === undefined) {
+      return 0;
+    }
+
+    if (age > 64) {
+      return Math.ceil(plan.cost * 0.4);
+    }
+
+    return plan.cost;
+  }
+
+  let reduction = compute_day_of_week_discount(holidays, date);
+
+  // TODO apply reduction for others
+  if (age === undefined) {
+    let cost = plan.cost * (1 - reduction / 100);
+    return Math.ceil(cost);
+  }
+
+  if (age < 15) {
+    return Math.ceil(plan.cost * 0.7);
+  }
+
+  if (age > 64) {
+    let cost = plan.cost * 0.75 * (1 - reduction / 100);
+    return Math.ceil(cost);
+  }
+
+  let cost = plan.cost * (1 - reduction / 100);
+  return Math.ceil(cost);
+}
+
 async function createApp() {
   const app = express();
 
@@ -61,50 +99,10 @@ async function createApp() {
         [data.type]
       )
     )[0][0];
-
-    if (data.age < 6) {
-      res.json({ cost: 0 });
-      return;
-    }
-
-    if (data.type === "night") {
-      if (data.age >= 6) {
-        if (data.age > 64) {
-          res.json({ cost: Math.ceil(result.cost * 0.4) });
-        } else {
-          res.json(result);
-        }
-      } else {
-        res.json({ cost: 0 });
-      }
-      return;
-    }
-
     const holidays = (await connection.query("SELECT * FROM `holidays`"))[0];
-    let reduction = compute_day_of_week_discount(holidays, data.date);
 
-    // TODO apply reduction for others
-    if (data.age === undefined) {
-      let cost = result.cost * (1 - reduction / 100);
-      res.json({ cost: Math.ceil(cost) });
-      return;
-    }
-
-    if (data.age < 15) {
-      res.json({ cost: Math.ceil(result.cost * 0.7) });
-      return;
-    }
-
-    if (data.age > 64) {
-      let cost = result.cost * 0.75 * (1 - reduction / 100);
-      res.json({ cost: Math.ceil(cost) });
-      return;
-    }
-
-    let cost = result.cost * (1 - reduction / 100);
-    res.json({ cost: Math.ceil(cost) });
-
-    return;
+    const final_price = compute_final_price(holidays, result, data);
+    return res.json({ ...result, cost: final_price });
   });
 
   return { app, connection };
